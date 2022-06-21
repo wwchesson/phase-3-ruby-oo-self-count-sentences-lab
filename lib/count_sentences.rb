ca644388b09f6125489bac5fa700029d880014f4
Will <willchesson@gmail.com>
2022-06-21 11:54:59 -0400

    commit tree c7adff36dcd6bdba6ccaa7f9c709ecc3d73785f8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
+  # def count_sentences
+  #   self.split(/\.|\?|!/).count{|sentence| !sentence.empty? }
+  # end
 
   def count_sentences
-
+    sentences = 0
+    punctuation = [".", "!", "?"]
+    self.chars.each_with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
+
 end
